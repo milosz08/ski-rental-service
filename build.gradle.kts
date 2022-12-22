fdd2a8f466989347aad64906870dbdeb292639de
Miłosz Gilga <business@miloszgilga.pl>
2022-12-22 22:40:28 +0100

    minor fixes

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,10 @@
 /*
- * Copyright (c) 2022 by MILOSZ GILGA and FILIP HABRYN
+ * Copyright (c) 2022 by multiple authors
  * Silesian University of Technology
  *
  *  File name: build.gradle.kts
- *  Last modified: 20/12/2022, 10:35
- *  Project name: ski-rent-service
+ *  Last modified: 22.12.2022, 22:24
+ *  Project name: ski-rental-service
  *
  * This project was written for the purpose of a subject taken in the study of Computer Science.
  * This project is not commercial in any way and does not represent a viable business model
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,10 +1,10 @@
 /*
- * Copyright (c) 2022 by MILOSZ GILGA and FILIP HABRYN
+ * Copyright (c) 2022 by multiple authors
  * Silesian University of Technology
  *
  *  File name: settings.gradle.kts
- *  Last modified: 20/12/2022, 10:03
- *  Project name: ski-rent-service
+ *  Last modified: 20.12.2022, 16:39
+ *  Project name: ski-rental-service
  *
  * This project was written for the purpose of a subject taken in the study of Computer Science.
  * This project is not commercial in any way and does not represent a viable business model
