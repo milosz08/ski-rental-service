640b257567b7d23c7f4fe1090712d8e42494f1ea
Miłosz Gilga <business@miloszgilga.pl>
2022-12-22 22:21:06 +0100

    minor changes and update copyright header

--- src/main/java/pl/polsl/skirentservice/domain/HelloServlet.java
+++ src/main/java/pl/polsl/skirentservice/domain/HelloServlet.java
@@ -1,10 +1,10 @@
 /*
- * Copyright (c) 2022 by MILOSZ GILGA and FILIP HABRYN
+ * Copyright (c) 2022 by multiple authors
  * Silesian University of Technology
  *
  *  File name: HelloServlet.java
- *  Last modified: 20/12/2022, 10:22
- *  Project name: ski-rent-service
+ *  Last modified: 22.12.2022, 20:19
+ *  Project name: ski-rental-service
  *
  * This project was written for the purpose of a subject taken in the study of Computer Science.
  * This project is not commercial in any way and does not represent a viable business model
