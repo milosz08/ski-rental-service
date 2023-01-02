53af523a3a115032ba40ce73099c995ee21555ff
Miłosz Gilga <gilgamilosz451@gmail.com>
2023-01-02 01:29:39 +0100

    rewrite guard filer into two separate filters, one guard protected routes and another guard non-protected routes

--- src/main/java/pl/polsl/skirentalservice/filter/RedirectNonProtectedRoutesFilter.java
+++ src/main/java/pl/polsl/skirentalservice/filter/RedirectNonProtectedRoutesFilter.java
@@ -2,8 +2,8 @@
  * Copyright (c) 2022 by multiple authors
  * Silesian University of Technology
  *
- *  File name: ProtectedRoutesGuardFilter.java
- *  Last modified: 28/12/2022, 03:43
+ *  File name: RedirectNonProtectedRoutesFilter.java
+ *  Last modified: 31/12/2022, 17:17
  *  Project name: ski-rental-service
  *
  * This project was written for the purpose of a subject taken in the study of Computer Science.
@@ -17,22 +17,28 @@ import jakarta.servlet.*;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
 
+import pl.polsl.skirentalservice.dto.login.LoggedUserDataDto;
+
 import java.util.Objects;
 import java.io.IOException;
 
+import static pl.polsl.skirentalservice.util.UserRole.OWNER;
 import static pl.polsl.skirentalservice.util.SessionAttribute.LOGGED_USER_DETAILS;
 
 //----------------------------------------------------------------------------------------------------------------------
 
-@WebFilter(urlPatterns = { "/seller/*", "/owner/*" }, initParams = @WebInitParam(name = "mood", value = "awake"))
-public class ProtectedRoutesGuardFilter extends HttpFilter {
+@WebFilter(urlPatterns = { "/login", "/forgot-password-request", "/change-forgotten-password/*" },
+        initParams = @WebInitParam(name = "mood", value = "awake"))
+public class RedirectNonProtectedRoutesFilter extends HttpFilter {
 
     @Override
     protected void doFilter(HttpServletRequest req, HttpServletResponse res, FilterChain chain)
             throws IOException, ServletException {
         final HttpSession httpSession = req.getSession();
-        if (Objects.isNull(httpSession.getAttribute(LOGGED_USER_DETAILS.getName()))) {
-            res.sendRedirect("/login");
+        final var loggedUserDetailsDto = (LoggedUserDataDto) httpSession.getAttribute(LOGGED_USER_DETAILS.getName());
+        if (!Objects.isNull(loggedUserDetailsDto)) {
+            final String redirect = loggedUserDetailsDto.getRoleAlias().equals(OWNER.getAlias()) ? "owner" : "seller";
+            res.sendRedirect("/" + redirect + "/dashboard");
             return;
         }
         chain.doFilter(req, res);
