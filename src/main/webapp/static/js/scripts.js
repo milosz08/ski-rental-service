e4580151a87468d4a381bf2145607dc49de6aa55
milosz08 <gilgamilosz451@gmail.com>
2023-06-08 15:00:43 +0200

    change to Apache 2.0 license

--- src/main/webapp/static/js/scripts.js
+++ src/main/webapp/static/js/scripts.js
@@ -1,14 +1,19 @@
 /*
- * Copyright (c) 2022 by multiple authors
- * Silesian University of Technology
+ * Copyright (c) 2023 by MILOSZ GILGA <http://miloszgilga.pl>
  *
- *  File name: scripts.js
- *  Last modified: 28/12/2022, 23:29
- *  Project name: ski-rental-service
+ * File name: scripts.js
+ * Last modified: 2/21/23, 1:17 AM
+ * Project name: ski-rental-service
  *
- * This project was written for the purpose of a subject taken in the study of Computer Science.
- * This project is not commercial in any way and does not represent a viable business model
- * of the application. Project created for educational purposes only.
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this
+ * file except in compliance with the License. You may obtain a copy of the License at
+ *
+ *     <http://www.apache.org/license/LICENSE-2.0>
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS
+ * OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the license.
  */
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
