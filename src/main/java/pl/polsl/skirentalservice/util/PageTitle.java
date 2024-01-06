0bc9c62e54a738caa156c6b993976e390779096e
milosz08 <gilgamilosz451@gmail.com>
2024-01-06 11:44:26 +0100

    add page showing for internal server error (500)

--- src/main/java/pl/polsl/skirentalservice/util/PageTitle.java
+++ src/main/java/pl/polsl/skirentalservice/util/PageTitle.java
@@ -11,6 +11,7 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum PageTitle {
     NOT_FOUND_4O4_PAGE("404"),
+    INTERNAL_SERVER_ERROR_PAGE("500"),
     LOGIN_PAGE("Logowanie"),
     FIRST_ACCESS_PAGE("Inicjalizacja konta"),
     FORGOT_PASSWORD_REQUEST_PAGE("Resetuj hasło"),
