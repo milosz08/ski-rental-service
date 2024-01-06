9727c7f24cfdb3acd52a071920e96658a808109e
milosz08 <gilgamilosz451@gmail.com>
2024-01-06 11:43:39 +0100

    add page showing for internal server error (500)

---
+++ src/main/java/pl/polsl/skirentalservice/domain/InternalServerErrorServlet.java
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2023 by MILOSZ GILGA <https://miloszgilga.pl>
+ * Silesian University of Technology
+ */
+package pl.polsl.skirentalservice.domain;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import pl.polsl.skirentalservice.util.PageTitle;
+
+import java.io.IOException;
+
+@WebServlet("/500")
+public class InternalServerErrorServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+        req.setAttribute("title", PageTitle.INTERNAL_SERVER_ERROR_PAGE.getName());
+        req.getRequestDispatcher("/WEB-INF/pages/_internal-server-error.jsp").forward(req, res);
+    }
+}
